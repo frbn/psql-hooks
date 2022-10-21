4a75b12b9b44389cf6c0f95ff7ca2b4555c2311b
Franck Boudehen <franck.boudehen@dalibo.com>
2022-10-21 14:03:13 +0200

    add planner hook

---
+++ extension/planner.c
@@ -0,0 +1,24 @@
+ #include "optimizer/planner.h"
+
+
+static planner_hook_type ah_original_planner_hook  = NULL;
+
+
+static PlannedStmt *
+ah_planner_hook(Query *parse, const char *query_st, int cursorOptions, ParamListInfo boundp)
+{
+    PlannedStmt *result;
+    
+    elog(WARNING, "planner hook called");
+	
+    if (ah_original_planner_hook){
+		result = ah_original_planner_hook(parse,query_st,cursorOptions, boundp);
+	}
+	else
+	{
+		result = standard_planner(parse, query_st, cursorOptions, boundp);
+	}
+	return result;
+
+
+}
