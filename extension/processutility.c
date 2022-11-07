6fbb924eab3dc23d4c02034ef702fd61c00d6784
Franck Boudehen <franck.boudehen@dalibo.com>
2022-11-07 16:35:22 +0100

    add processUtility hook

--- extension/processutility.c
+++ extension/processutility.c
@@ -5,11 +5,15 @@
 static ProcessUtility_hook_type ah_original_ProcessUtility_hook = NULL;
 
 
-static void ah_process_utility(PlannedStmt *pstmt, const char *queryString,ProcessUtilityContext context,ParamListInfo params,QueryEnvironment *queryEnv,DestReceiver *dest,       char *completionTag){
+static void ah_process_utility(PlannedStmt *pstmt, const char *queryString,bool readOnlyTree, ProcessUtilityContext context,ParamListInfo params,QueryEnvironment *queryEnv,DestReceiver *dest,QueryCompletion *completionTag){
 
         elog(WARNING,"ProcessUtility hook called");
-    
-    
-    
+		if (ah_original_ProcessUtility_hook){
+			ah_original_ProcessUtility_hook(pstmt, queryString,readOnlyTree,context,params,queryEnv,dest, completionTag);    
+    	}else{
+			standard_ProcessUtility(pstmt,queryString, readOnlyTree, context, params, queryEnv, dest, completionTag);
+
+		}
+		
 }
 
