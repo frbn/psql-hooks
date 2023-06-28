7b7af2424f057968467f135490a9f04c50e266f9
Franck Boudehen <franck.boudehen@dalibo.com>
2023-06-28 18:32:24 +0200

    add run start finish

--- extension/all_hooks.c
+++ extension/all_hooks.c
@@ -6,8 +6,24 @@
 //ClientAuthentication_hook
 #include "auth.c"
 
+
+// Executor
+
+// executeCheckPerms_hook
+#include "executorcheckperms.c"
+
+//ExecutorStart_hook
+#include "executor_start.c"
+
+//ExecutorRun_hook
+#include "executor_run.c"
+
+//ExecutorFinish_hook
+#include "executor_finish.c"
+
 //ExecutorEnd_hook
 #include "executor_end.c"
+
 
 // planner_hook
 #include "planner.c"
@@ -15,8 +31,6 @@
 // ProcessUtility_hook
 #include "processutility.c"
 
-// executeCheckPerms_hook
-#include "executorcheckperms.c"
 
 // emit_log_hook
 #include "emitlog.c"
@@ -38,7 +52,7 @@ void _PG_init(void)
     elog(WARNING, "all_hooks init");
     //--------------------------
     // ClientAuthentication_hook
-    
+
     // Save the original hook value.
 	elog(WARNING,"hooking: ClientAuthentication_hook");
     ah_original_client_auth_hook = ClientAuthentication_hook;
@@ -46,7 +60,7 @@ void _PG_init(void)
     // Register our handler.
     ClientAuthentication_hook = ah_auth_delay_checks;
     //--------------------------
-    
+
     //--------------------------
     // ExecutorEnd_hook
 	elog(WARNING,"hooking: emit_log_hook");
@@ -61,36 +75,50 @@ void _PG_init(void)
         ah_original_planner_hook = planner_hook;
         planner_hook = ah_planner_hook;
     }
-    //--------------------------    
+    //--------------------------
     // ProcessUtility_hook
-    if (ProcessUtility_hook != ah_process_utility){        
+    if (ProcessUtility_hook != ah_process_utility){
 		elog(WARNING,"hooking: ProcessUtility_hook");
         ah_original_ProcessUtility_hook = ProcessUtility_hook;
         ProcessUtility_hook = ah_process_utility;
     }
-    
-    
-    //--------------------------    
+
+
+    //--------------------------
+    // ExecutorStart_hook
+    if (ExecutorStart_hook != ah_ExecutorStart_hook){
+  		elog(WARNING,"hooking: ExecutorStart_hook");
+  		ah_original_ExecutorStart_hook = ExecutorStart_hook;
+  		ExecutorStart_hook = ah_ExecutorStart_hook;
+  	}
+
+    //ExecutorRun_hook
+    if (ExecutorRun_hook != ah_ExecutorRun_hook){
+      elog(WARNING,"hooking: ExecutorRun_hook");
+      ah_original_ExecutorRun_hook = ExecutorRun_hook;
+      ExecutorRun_hook = ah_ExecutorRun_hook;
+    }
+
 	// ExecutorCheckPerms_hook
 	if (ExecutorCheckPerms_hook != ah_ExecutorCheckPerms_hook){
 		elog(WARNING,"hooking: ExecutorCheckPerms_hook");
 		ah_original_ExecutorCheckPerms_hook = ExecutorCheckPerms_hook;
 		ExecutorCheckPerms_hook = ah_ExecutorCheckPerms_hook;
 	}
-    
-    //--------------------------    
+
+    //--------------------------
 	// emit_log_hook
 	if (emit_log_hook != ah_original_emit_log_hook){
 		elog(WARNING,"hooking: hooking emit_log");
 		ah_original_emit_log_hook = emit_log_hook;
 		emit_log_hook = ah_emit_log_hook;
-	}    
-    //--------------------------    
-    
-    //--------------------------    
-    
-    
-    //--------------------------    
+	}
+    //--------------------------
+
+    //--------------------------
+
+
+    //--------------------------
 }
 
 // Called with extension unload.
@@ -102,21 +130,21 @@ void _PG_fini(void)
     // Return back the original hook value.
     ClientAuthentication_hook = ah_original_client_auth_hook;
     //--------------------------
-    
+
     //--------------------------
     // ExecutorEnd_hook
     ExecutorEnd_hook = ah_original_ExecutorEnd;
-    
+
     //--------------------------
 
     //--------------------------
     // planner_hook
     planner_hook = ah_original_planner_hook;
-    
+
     //--------------------------
     // ProcessUtility_hook
     ProcessUtility_hook = ah_original_ProcessUtility_hook;
-    
+
     //--------------------------
 	// ExecutoCheckPerms_hook
 	ExecutorCheckPerms_hook = ah_original_ExecutorCheckPerms_hook;
