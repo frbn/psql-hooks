262e86649075c8b076adefa70944efc493dad165
Franck Boudehen <franck.boudehen@dalibo.com>
2023-07-01 09:26:52 +0200

    emitlog renamed to emit_log

--- extension/emit_log.c
+++ extension/emit_log.c
@@ -1,11 +1,10 @@
 #include "utils/elog.h"
 
-emit_log_hook_type ah_original_emit_log_hook = NULL;
-static void ah_emit_log_hook(ErrorData* );
+static emit_log_hook_type ah_original_emit_log_hook = NULL;
 
+void ah_emit_log_hook(ErrorData* );
 
-
-static void ah_emit_log_hook(ErrorData * eData){
+void ah_emit_log_hook(ErrorData * eData){
 
 //TODO : explain why it doesn't work
 	elog(WARNING, "emit_log_hook called");
