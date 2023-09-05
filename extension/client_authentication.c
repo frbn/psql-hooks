3455ee89928c8440f49129035208d488c51e2c81
Franck Boudehen <franck.boudehen@dalibo.com>
2023-09-05 17:55:40 +0200

    mod clientauthentication_hook to return status

--- extension/client_authentication.c
+++ extension/client_authentication.c
@@ -6,6 +6,17 @@ static void ah_ClientAuthentication_hook(Port * port, int status);
 
 void ah_ClientAuthentication_hook(Port * port, int status){
 
-  elog(WARNING,"ah_ClientAuthentication_hook called");
+   // If any other extension registered its own hook handler,
+    // call it before performing our own logic.
+    if (ah_original_client_authentication_hook)
+        ah_original_client_authentication_hook(port, status);
 
+    // If authentication failed, we wait for one second before returning
+    // control to the caller.
+    if (status != STATUS_OK)
+    {
+      elog(WARNING,"ah_ClientAuthentication_hook status KO");
+    }else{
+      elog(WARNING,"ah_ClientAuthentication_hook called OK");
+    }
 }
