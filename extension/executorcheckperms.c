43d966216633379d1cc0ae8d4d9ea8429fdef55c
Franck Boudehen <franck.boudehen@dalibo.com>
2024-09-11 18:49:35 +0200

    wip

--- extension/executorcheckperms.c
+++ extension/executorcheckperms.c
@@ -1,7 +1,7 @@
 #include "executor/executor.h"
 
 static ExecutorCheckPerms_hook_type ah_original_ExecutorCheckPerms_hook = NULL;
-bool ah_ExecutorCheckPerms_hook (List* , bool );
+ExecutorCheckPerms_hook_type ah_ExecutorCheckPerms_hook (List* , bool );
 
 bool ah_ExecutorCheckPerms_hook (List* tableList, bool abort){
 
