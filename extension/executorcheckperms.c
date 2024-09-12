1f65a16053349a63c12ff6979b00aac3d798af98
Franck Boudehen <franck.boudehen@dalibo.com>
2024-09-12 17:16:13 +0200

    prototype changed in v16

--- extension/executorcheckperms.c
+++ extension/executorcheckperms.c
@@ -1,9 +1,9 @@
 #include "executor/executor.h"
 
 static ExecutorCheckPerms_hook_type ah_original_ExecutorCheckPerms_hook = NULL;
-static bool ah_ExecutorCheckPerms_hook (List*, bool );
+static bool ah_ExecutorCheckPerms_hook (List*, List* , bool );
 
-static bool ah_ExecutorCheckPerms_hook (List* tableList, bool abort){
+static bool ah_ExecutorCheckPerms_hook (List* tableList, List* rteperminfos, bool abort){
 
 	elog(DEBUG1, "ExecutorCheckPerms_hook called");
 
